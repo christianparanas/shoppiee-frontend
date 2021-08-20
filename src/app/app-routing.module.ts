8ac45c01f1df8e854ff89df7a5732c16c4d3a738
Christian Paranas <christiannparanas@gmail.com>
2021-08-20 16:09:22 +0800

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { NavComponent } from './nav/nav.component';
+
 const routes: Routes = [];
 
 @NgModule({
