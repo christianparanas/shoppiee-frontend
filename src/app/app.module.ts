19df3161586eb1f3eff6c61ca5b3e54a5dd71b9a
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2021-08-28 14:42:15 -0700

    creating LoginPage Directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { LogInPageComponent } from './log-in-page/log-in-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    LogInPageComponent
   ],
   imports: [
     BrowserModule,
