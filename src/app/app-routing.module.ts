f6b374f7feaa6b9aadfce0a1c7dfcb02dd7de58d
christianparanas <christiannparanas@gmail.com>
2021-08-29 09:01:07 +0800

    Init register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
-  
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
