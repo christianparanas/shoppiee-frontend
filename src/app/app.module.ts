b72844763770ee59a5a81c435f0479e07354cbf1
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2021-08-29 00:30:24 -0700

    login page interface first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { LogInPageComponent } from './log-in-page/log-in-page.component';
+import { LogInPageComponent } from './pages/log-in-page/log-in-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    LogInPageComponent
+    LogInPageComponent,
+    HeaderComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
