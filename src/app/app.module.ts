9b67c20eb90cb283078e5d6c405a2c635a7536c2
christianparanas <christiannparanas@gmail.com>
2021-08-29 09:15:23 +0800

    register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
 
   ],
   imports: [
