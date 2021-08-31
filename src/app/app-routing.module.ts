67b48c4e7055e6f24caa4512b0b5068c83b1777e
christianparanas <christiannparanas@gmail.com>
2021-08-31 09:47:30 +0800

    categPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component'
 import { RegisterComponent } from './pages/register/register.component';
 import { LogInPageComponent } from './pages/log-in-page/log-in-page.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'categories', component: CategoriesComponent },
   { path: 'account/register', component: RegisterComponent },
   { path: 'account/login', component: LogInPageComponent },
 ];
