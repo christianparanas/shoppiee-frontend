1f5032fb7a4670960b6592947e400fde15eea43d
christianparanas <christiannparanas@gmail.com>
2021-09-01 12:26:10 +0800

    accountRoutte

---
+++ src/app/pages/account/account.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {RouterModule,Router} from '@angular/router'
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.scss']
+})
+export class AccountComponent implements OnInit {
+  userIsAuth: boolean = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    if(this.userIsAuth == false) {
+      this.router.navigate(["/account/login"]);
+    }
+  }
+
+}
