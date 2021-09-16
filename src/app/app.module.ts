60c0e28eef40a843b505c7746970f658674cccfc
christianparanas <christiannparanas@gmail.com>
2021-09-16 13:48:57 +0800

    inject http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -58,6 +60,7 @@ import { StorepreviewComponent } from './components/storepreview/storepreview.co
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     NgxSkeletonLoaderModule,
     NgxSplideModule
   ],
