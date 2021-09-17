86c635be0943ac52b6b3e42f6cd961ced13687ba
Ruel Baldo <bebebaldo75@gmail.com>
2021-09-17 12:12:13 +0800

    add

--- src/app/pages/userstore/userstore.component.ts
+++ src/app/pages/userstore/userstore.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userstore.component.scss']
 })
 export class UserstoreComponent implements OnInit {
-
+  newProductsArr = new Array(7)
+  topProductsArr = new Array(7)
+  discoverProductsArr = new Array(22)
+  
   constructor() { }
 
   ngOnInit(): void {
