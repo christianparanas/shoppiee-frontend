a5413a6e491f97b6ba0999a4bf3032655be04b40
christianparanas <christiannparanas@gmail.com>
2021-09-20 19:11:54 +0800

    test

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -27,7 +27,6 @@ export class NavComponent implements OnInit {
     this.isSideBarOpen =! this.isSideBarOpen;
   }
 
-
   listenScrollEvent = () => {
     window.scrollY > 15 ? this.onScroll = true : this.onScroll = false;
   }
