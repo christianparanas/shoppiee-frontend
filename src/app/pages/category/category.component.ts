ecd80ff2ca51e11c2553d8179341919ae5440d5e
christianparanas <christiannparanas@gmail.com>
2021-09-28 15:55:28 +0800

    added category page

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
   isImgLoaded: boolean = false;
+  categoryProductsArr = new Array(11);
 
   constructor() {}
 
