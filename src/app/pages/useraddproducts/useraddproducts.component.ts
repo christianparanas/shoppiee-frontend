899c1c4341322eb453c3ee502d9567bb168601f4
Ruelbaldo <bebebaldo75@gmail.com>
2021-09-30 15:15:01 +0800

    Minimize

---
+++ src/app/pages/useraddproducts/useraddproducts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-useraddproducts',
+  templateUrl: './useraddproducts.component.html',
+  styleUrls: ['./useraddproducts.component.scss']
+})
+export class UseraddproductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
