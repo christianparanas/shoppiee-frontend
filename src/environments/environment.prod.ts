a07c0c2dc6656e994a4a289e4bf5a0572af3ed77
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2021-10-20 10:39:02 -0700

    adding backend #2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseURL: 'http://localhost:8000'
+  // baseURL: 'http://localhost:8000'
+  baseURL: 'https://still-forest-97994.herokuapp.com'
 };
