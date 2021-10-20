28166782f8f1da58f54a44ca1a527c123e6d01f5
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2021-10-20 09:29:46 -0700

    minimizing the logic of notification in login-page

--- src/app/pages/log-in-page/log-in-page.component.ts
+++ src/app/pages/log-in-page/log-in-page.component.ts
@@ -69,18 +69,19 @@ export class LogInPageComponent implements OnInit {
         },
         (error) => {
           this.submitLoading = false;
+          let result:any;
 
           if (error.status == 0) {
-            this.toast.error('Server is down, help! hehe', {
-              position: 'top-right',
-            });
+            result='Server is down, help! hehe'
           } else if (error.status == 401) {
-            this.toast.error(error.error.message, { position: 'top-right' });
+            result=error.error.message
           } else if (error.status == 422) {
-            this.toast.error(error.error.email[0], { position: 'top-right' });
+            result=error.error.email[0]
           } else if (error.status == 500) {
-            this.toast.error(error.statusText, { position: 'top-right' });
+            result=error.statusText
           }
+
+          this.toast.error(result,{ position: 'top-right' })
 
           console.log(error);
         }
