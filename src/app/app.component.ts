43cef5dc947dcf4ad1112760613a6d16d93dae20
christianparanas <christiannparanas@gmail.com>
2021-10-20 16:33:10 +0800

    ch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'learnangular';
-
-  
-}
+export class AppComponent { }
